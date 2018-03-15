ceeccad5f4098e4b7ac735031a9479e94a2f844e
Martin Katreník <martin.katrenik@gmail.com>
2018-03-15 10:30:29 +0100

    feat(server): add cors pkg

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 import { ImagesModule } from './images/images.module'
+import * as cors from 'cors'
 
 async function bootstrap() {
   const app = await NestFactory.create(ImagesModule)
   app.useGlobalPipes(new ValidationPipe({ transform: true }))
+  app.use(cors())
   await app.listen(3000)
 }
 bootstrap()
