793e8d015e809fb11f3b9563524968e309a6e5fe
Martin Katreník <martin.katrenik@gmail.com>
2018-03-15 09:42:14 +0100

    feat(server:images): make file upload fields optional except image

--- server/src/images/dto/create-image-payload.ts
+++ server/src/images/dto/create-image-payload.ts
@@ -1,7 +1,15 @@
-import { IsString } from 'class-validator'
+import { IsString, IsOptional } from 'class-validator'
 
 export class CreateImagePayload {
-  @IsString() name: string
-  @IsString() title: string
-  @IsString() description: string
+  @IsString()
+  @IsOptional()
+  name: string
+
+  @IsString()
+  @IsOptional()
+  title: string
+
+  @IsString()
+  @IsOptional()
+  description: string
 }
