e0075297eb838c3724343be150613934aae2af35
Martin Katreník <martin.katrenik@gmail.com>
2018-03-18 18:44:44 +0100

    fix(server:images): fix default album id & deletehash

--- server/src/images/images.providers.ts
+++ server/src/images/images.providers.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 
 const IMGUR_CLIENT_ID = 'e19091a8f355573'
 
-export const DEFAULT_ALBUM_ID = 'f0ZKD'
-export const DEFAULT_ALBUM_DELETE_HASH = 'lsOPwbafi63Y7kS'
+export const DEFAULT_ALBUM_ID = 'K5Ror'
+export const DEFAULT_ALBUM_DELETE_HASH = 'mkEtPZ3agJ6mydJ'
 
 axios.defaults.baseURL = 'https://api.imgur.com/3'
 axios.defaults.headers.common.Authorization = `Client-ID ${IMGUR_CLIENT_ID}`
